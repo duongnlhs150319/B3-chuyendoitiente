b3cffdb263ef2f54ce1efe36b3b955ce08af82c0
duongnlhs150319 <duongnlhs150319@gmail.com>
2025-05-13 01:51:27 +0700

    chuyen doi tien te

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿public class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Tỉ giá hôm nay là 26.000 VND/USD");
+        Console.Write("Nhập số USD mà bạn muốn quy đổi: ");
+        int rate = Convert.ToInt32(Console.ReadLine());
+        int convertRate = rate * 26000;
+        Console.WriteLine("Số tiền quy đổi ra VND là: " + convertRate + "đ");
+    }
+}
